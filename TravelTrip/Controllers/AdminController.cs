8a29ff7d46924b5fb99cafd237cc008ed1f65250
Muhammed Murat ACAR <muhammedmuratacarr@gmail.com>
2021-05-24 22:21:12 +0300

    TravelTripBlog
    TravelTripBlog

---
+++ TravelTrip/Controllers/AdminController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+namespace TravelTrip.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniBlog(Blog b)
+        {
+            c.Blogs.Add(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult BlogSil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BlogGetirP(int id)
+        {
+            var b = c.Blogs.Find(id);
+            return PartialView(b);
+        }
+
+     
+        public ActionResult BlogGetir(int id)
+        {
+            var b = c.Blogs.Find(id);
+            return View("BlogGetir",b);
+            //return PartialView(c.Blogs.FirstOrDefault(x => x.ID == id));
+
+        }
+
+        public ActionResult PartialViewEdit(int id)
+        {
+            Blog pr = new Blog();
+            pr =c.Blogs.Find(id);
+            return PartialView("PartialViewEdit", pr);
+        }
+
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.Aciklama = b.Aciklama;
+            blg.Baslik = b.Baslik;
+            blg.BlogImage = b.BlogImage;
+            blg.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [Authorize]
+
+        public ActionResult YorumListele()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+
+        public ActionResult YorumSil(int id)
+        {
+            var yorum = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(yorum);
+            c.SaveChanges();
+            return RedirectToAction("YorumListele");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            return View("YorumGetir", b);
+            //return PartialView(c.Blogs.FirstOrDefault(x => x.ID == id));
+
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yorum = c.Yorumlars.Find(y.ID);
+            yorum.KullaniciAdi = y.KullaniciAdi;
+            yorum.Mail = y.Mail;
+            yorum.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("YorumListele");
+
+        }
+    }
+}
---
+++ TravelTrip/Controllers/DefaultController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+namespace TravelTrip.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.Take(4).ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var degeri = c.Blogs.OrderByDescending(x => x.ID).Take(10).ToList();
+            return PartialView(degeri);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            return PartialView(deger);
+        }
+
+    }
+}
